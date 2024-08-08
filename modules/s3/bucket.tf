fc19efb18f35d57da9e263e18f83df1fb1fdbf0e
christian-calabrese <christian.calabrese@outlook.com>
2024-08-08 09:56:21 +0200

    Update bucket.tf

--- modules/s3/bucket.tf
+++ modules/s3/bucket.tf
@@ -1,8 +1,6 @@
 resource "aws_s3_bucket" "this" {
   #checkov:skip=CKV2_AWS_61: "This is a demo bucket"
   #checkov:skip=CKV2_AWS_61: "This is a demo bucket"
-  #checkov:skip=CKV2_AWS_61: "This is a demo bucket"
-
   bucket = var.bucket_name
 }
 
